1612c369a2c46c6aa05bf80a69f8c00fded00155
JulianoCeconi <130612743+JulianoCeconi@users.noreply.github.com>
2023-06-07 20:20:10 -0300

    Add files via upload
    Parte 2

--- Trabalho Ponto Extra - 2° Bim/scripts/exercicio4.js
+++ Trabalho Ponto Extra - 2° Bim/scripts/exercicio4.js
@@ -4,7 +4,17 @@ let btCalcular = document.querySelector("#btCalcular");
 let h2Resultado = document.querySelector("#h2Resultado");
 
 function calculaTaxa(){
+    let vlr1 = Number(inputNumero1.value);
+    let vlr2 = Number(inputNumero2.value);
 
+    let valor1 = vlr2 * (1/100);
+    let valor2 = vlr2 * (1.5/100);
+
+    if(vlr1 <= 1990){
+        h2Resultado.innerHTML = "A taxa &eacute; de: " +valor1;
+    }else if(vlr1 > 1990){
+        h2Resultado.innerHTML = "A taxa &eacute; de: "+valor2;
+    }else{}
 
 }
 
--- Trabalho Ponto Extra - 2° Bim/scripts/exercicio5.js
+++ Trabalho Ponto Extra - 2° Bim/scripts/exercicio5.js
@@ -1,11 +1,24 @@
-let inputNumero1 = document.querySelector("#inputNumero1");
-let inputNumero2 = document.querySelector("#inputNumero2");
+let inputSalario = document.querySelector("#inputSalario");
+let inputCargo = document.querySelector("#inputCargo");
 let btCalcular = document.querySelector("#btCalcular");
 let h2Resultado = document.querySelector("#h2Resultado");
 
 function aumentoSalario(){
+    let salario = Number(inputSalario.value);
+    let cargo = String(inputCargo.value);
 
+    let porcentagem10 = salario * (10/100);
+    let porcentagem20 = salario * (20/100);
+    let porcentagem30 = salario * (30/100);
+    let porcentagem40 = salario * (40/100);
 
+    if(cargo = "Gerente"){
+        h2Resultado.innerHTML = "O aumento do gerente &eacute; " +porcentagem10;
+    }else if(cargo = "Engenheiro"){
+        h2Resultado.innerHTML = "O aumento do engenheiro &eacute; " +porcentagem20;
+    }else if(cargo = "Tecnico"){
+        h2Resultado.innerHTML = "O aumento do T&eacute;cnico: " + porcentagem40; 
+    }else(cargo)
 }
 
 btCalcular.onclick = function(){
